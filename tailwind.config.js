c9848138ff9189305cf8e17b0c142739373750a3
vladakosha <koshavlada@gmail.com>
2023-11-15 16:23:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#2D333E',
+        'light-blue': '#94A9C9',
+        'red': '#BB4251',
+        'nude': '#C9ADA7',
+      },
+      fontFamily: {
+      'paragraph': ['Open Sans', 'ui-sans-serif', 'system-ui'],
+      'heading': ['Bell-MT', 'ui-serif', 'Georgia'],
+    }
+  },
+  plugins: [],
+}
+}
