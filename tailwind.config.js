542559423a37101133bdd37c81e07a561eb2e418
vladakosha <koshavlada@gmail.com>
2023-11-17 16:34:50 +0100

    updated hero again

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./*"],
   theme: {
+    colors: {
+      "dark-blue": "#2D333E",
+      "light-blue": "#94A9C9",
+      "red": "#BB4251",
+      "nude": "#C9ADA7",
+    },
+    fontFamily: {
+      serif: ["Bell-MT", "ui-serif", "Georgia"],
+      sans: ["Open Sans", "ui-sans-serif", "system-ui"],
+    },
     extend: {
       backgroundImage: {
-        'hero-image': "url('/assets/hero-img.jpg')",
-
+        "hero-image": "url('/assets/hero-img.jpg')",
       },
-      colors: {
-        'dark-blue': '#2D333E',
-        'light-blue': '#94A9C9',
-        'red': '#BB4251',
-        'nude': '#C9ADA7',
-      },
-      fontFamily: {
-        'serif': ['Bell-MT', 'ui-serif', 'Georgia'],
-      'sans': ['Open Sans', 'ui-sans-serif', 'system-ui'],
-
-    }
-     
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-}
+};
